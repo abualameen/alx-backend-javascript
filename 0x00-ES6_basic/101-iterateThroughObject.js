cb4622ad31f3daaf89ec3bde1363536a2338259c
abu al ameen <abulyaqs@gmail.com>
2024-03-05 13:45:46 +0100

    correction

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
   let result = '';
-  
+
   // Iterate through the iterator object
   for (const employee of reportWithIterator) {
-    result += employee + ' | ';
+    result += `${employee} | `; // Use template literals instead of string concatenation
   }
-  
+
   // Remove the trailing ' | ' from the result string
   result = result.slice(0, -3);
-  
+
   return result;
 }
