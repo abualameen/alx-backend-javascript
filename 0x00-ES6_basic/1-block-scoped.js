90bce69bc0ef5911173bf078e0d7e8a08d136b40
abu al ameen <abulyaqs@gmail.com>
2024-03-05 13:10:04 +0100

    corrections

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
+  if (trueOrFalse) {
+    //
+  }
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-    // 
-  }
 
   return [task, task2]; // Return the outer variables
 }
