795eaa8674dff615401a237e3243fe3b23367b67
abu al ameen <abulyaqs@gmail.com>
2024-03-06 23:56:35 +0100

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error);
+    queue.push(error.message); // Push only the error message
   }
 
   queue.push('Guardrail was processed');
