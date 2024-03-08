3ea7ca6166a4a59e24b343ca0ece0b3e800b26eb
abu al ameen <abulyaqs@gmail.com>
2024-03-08 12:21:30 +0100

    correction

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -3,11 +3,7 @@ import Building from './5-building.js';
 const b = new Building(100);
 console.log(b);
 
-class TestBuilding extends Building {
-  static evacuationWarningMessage() {
-    console.log('Override method: evacuationWarningMessage');
-  }
-}
+class TestBuilding extends Building {}
 
 try {
     TestBuilding.evacuationWarningMessage();
