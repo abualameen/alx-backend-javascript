3f2a9504d73105b9a9544787df00ad7fb3d533d1
abu al ameen <abulyaqs@gmail.com>
2024-03-12 12:19:03 +0100

    correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-  const output = [];
-  if (startString.length === 0) {
-    return '';
-  }
+	const output = [];
+ 	if (startString === '') {
+		return '';
+	}
 
-  for (const things of set) {
-    if (things.startsWith(startString)) {
-      output.push(things.slice(startString.length));
-    }
-  }
+	for (const things of set) {
+		if (things.startsWith(startString)) {
+			output.push(things.slice(startString.length));
+		}
+	}
 
-  return output.join('-');
+	return output.join('-');
 }
