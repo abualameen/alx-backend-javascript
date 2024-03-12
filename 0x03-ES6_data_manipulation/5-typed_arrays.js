6e8d8a70d378e2583fe69ab57a2babe9fc1c9e0b
abu al ameen <abulyaqs@gmail.com>
2024-03-12 13:06:14 +0100

    correction for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,15 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Posotion outside range');
   }
-
+  if (!Number.isInteger(length) || length < 0) {
+    throw new Error('length must be an interger and positive.');
+  }
+  if (!Number.isInteger(position)) {
+    throw new Error('Postion must be an interger.');
+  }
+  if (!Number.isInteger(value)) {
+    throw new Error('Value must be an integer.');
+  }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   view.setInt8(position, value);
