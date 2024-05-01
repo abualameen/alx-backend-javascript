206004711687544b0bae970144a6d982f05d303d
abu al ameen <abulyaqs@gmail.com>
2024-05-01 11:58:55 +0100

    revisiting all task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,9 @@
 // Import the 'process' module
 const process = require('process');
-
-const phrase = 'Welcome to Holberton School, what is your name?';
+const phrase = "Welcome to Holberton School, what is your name?"
 
 function wrt() {
-  process.stdout.write(`${phrase}\n`);
+  process.stdout.write(`${phrase}`);
 }
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
-  process.exit();
-});
-process.on('exit', () => {
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
-});
-wrt();
+wrt()
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,18 +1,19 @@
 const http = require('http');
-
+const hostname = '127.0.0.1';
 const PORT = 1245;
 
 // Create the HTTP server
 const app = http.createServer((req, res) => {
   // Set the response headers
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   // Send the response body
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 // Listen for incoming requests on the specified port
-app.listen(PORT, () => {
+app.listen(PORT, hostname, () => {
   console.log(`Server is running and listening on port ${PORT}`);
 });
 
