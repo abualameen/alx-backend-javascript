a8e65775097aba0c65d938943a608b398c8d3161
abu al ameen <abulyaqs@gmail.com>
2024-05-01 15:55:20 +0100

    revisiting all task

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -7,12 +7,21 @@ const app = express();
 
 // Define a route for the root endpoint
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-// Define a route for all other endpoints
-app.use((req, res) => {
-  res.status(404).send(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ${req.url}</pre></body></html>`);
+// Define a catch-all route for all other endpoints
+app.get('*', (req, res) => {
+  res.status(404).send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`);
 });
 
 // Start the server and listen on the specified port
