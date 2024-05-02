5e6388ca06c18c9f38ff152de034cc35939eec89
abu al ameen <abulyaqs@gmail.com>
2024-05-02 23:43:30 +0100

    task 8,9 10

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const port = 7865;
+const localhost = 'localhost';
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-const server = app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+const server = app.listen(port, localhost, () => {
+  console.log(`API available on ${localhost} port ${port}`);
 });
 
 module.exports = server;
